94c435bd602624717a16845f81dce1d2dc8761cb
Samuel Coutinho <samuel_alves_coutinho@hotmail.com>
2020-06-19 17:37:27 -0300

    adicionada funcionalidade visual dos cards referente ao tempo de vencimento

--- lib/views/sended_codes_screen.dart
+++ lib/views/sended_codes_screen.dart
@@ -5,8 +5,6 @@ import 'package:flutter/material.dart';
 class SendedCodesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    final _date = DateTime.now();
     return Scaffold(
       appBar: AppBar(
         title: Text("Códigos TV Box"),
@@ -14,11 +12,7 @@ class SendedCodesScreen extends StatelessWidget {
         backgroundColor: Colors.redAccent[200],
       ),
       body: FutureBuilder<QuerySnapshot>(
-        future: Firestore.instance
-            .collection("codigos")
-            .document("1 mês")
-            .collection("items")
-            .getDocuments(),
+        future: Firestore.instance.collection("vendidos").getDocuments(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
             return Center(child: CircularProgressIndicator());
@@ -27,36 +21,33 @@ class SendedCodesScreen extends StatelessWidget {
               children: snapshot.data.documents
                   .map((doc) => Card(
                       elevation: 3,
-                      color: Colors.indigo[700],
+                      color: _color(doc.data["type"]),
                       margin:
                           EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
                       child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 10.0),
                         child: Row(
                           children: <Widget>[
                             Expanded(
                                 child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
                                 Text(
                                   "Código: ${doc.data["code"]}",
                                   style: TextStyle(
-                                      color: Colors.white, fontSize: 16.0),
+                                      color: Colors.black, fontSize: 16.0),
                                 ),
                                 Text(
                                   "Valor: R\$${doc.data["price"]}",
                                   style: TextStyle(
-                                      color: Colors.white, fontSize: 16.0),
+                                      color: Colors.black, fontSize: 16.0),
                                 ),
+                                _vencimento(doc.data["type"]),
                                 Text(
-                                  "Data de vencimento: ${_date.day}/${_date.month}/${_date.year}",
+                                  "Email: ${doc.data["email"]}",
                                   style: TextStyle(
-                                      color: Colors.white, fontSize: 16.0),
-                                ),
-                                Text(
-                                  "Email:",
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 16.0),
+                                      color: Colors.black, fontSize: 16.0),
                                 ),
                               ],
                             )),
@@ -72,5 +63,31 @@ class SendedCodesScreen extends StatelessWidget {
         },
       ),
     );
+  }
+
+  Color _color(String type) {
+    if (type == "1 ano")
+      return Colors.green[400];
+    else if (type == "3 meses") return Colors.yellow[400];
+    return Colors.red[400];
+  }
+
+  Text _vencimento(String type) {
+    final _date = DateTime.now();
+    final _mensal = _date.add(Duration(days: 30));
+    final _trimestral = _date.add(Duration(days: 90));
+    final _anual = _date.add(Duration(days: 365));
+    if (type == "1 ano") {
+      return Text(
+          "Data de vencimento: ${_anual.day}/${_anual.month}/${_anual.year}",
+          style: TextStyle(color: Colors.black, fontSize: 16.0));
+    } else if (type == "3 meses") {
+      return Text(
+          "Data de vencimento: ${_trimestral.day}/${_trimestral.month}/${_trimestral.year}",
+          style: TextStyle(color: Colors.black, fontSize: 16.0));
+    }
+    return Text(
+        "Data de vencimento: ${_mensal.day}/${_mensal.month}/${_mensal.year}",
+        style: TextStyle(color: Colors.black, fontSize: 16.0));
   }
 }
